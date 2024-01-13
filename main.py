61bb662367e3abb1715c3ebecd4c56e475a8df2e
nsanso <nsansoni97@gmail.com>
2024-01-13 17:09:42 +0100

    handle directories

--- main.py
+++ main.py
@@ -1,24 +1,84 @@
 #!/usr/bin/env python3
 
 import sys
-from os import path
+from pathlib import Path
 from PIL import Image, ImageFilter, ImageEnhance
 
 DEBUG = False
-# DEBUG = True
 
 
 def main():
-    [_, src, dest] = sys.argv
+    flags = set()
+    args = []
 
-    src = path.abspath(src)
-    dest = path.abspath(dest)
+    for arg in sys.argv:
+        if arg[0] == "-":
+            flags.add(arg)
+        else:
+            args.append(arg)
 
-    if src == dest:
-        raise Exception("cannot overwrite src")
+    if "--debug" in flags:
+        global DEBUG
+        DEBUG = True
 
-    pic = Image.open(src)
+    [_, src, dest] = args
 
+    src = Path(src).resolve()
+    dest = Path(dest).resolve()
+
+    if not src.exists():
+        raise Exception("Src doesn't exist")
+
+    if src.samefile(dest):
+        raise Exception("Can't overwrite src")
+
+    if src.is_dir():
+        if dest.is_file():
+            raise Exception(
+                "Can't use a file as destination when source is a directory"
+            )
+
+        dest.mkdir(parents=True, exist_ok=True)
+
+        for file in src.iterdir():
+            print(f"-> {file}")
+
+            if file.is_dir():
+                print(f"{file} is a directory")
+                continue
+
+            target = dest / file.name
+            if target.exists():
+                sys.stderr.write(f"Destination {target} already exists\n")
+                continue
+
+            try:
+                pic = Image.open(file)
+                out = apply_border(pic)
+                if not DEBUG:
+                    out.save(target)
+                    print(f"Saved as {target}")
+            except Exception as e:
+                sys.stderr.write(f"Failed to process {file}:\n\t{e}\n")
+
+    else:
+        if dest.is_dir():
+            dest = dest / src.name
+
+        if dest.exists():
+            raise Exception(f"Destination {dest} already exists")
+
+        dest.parent.mkdir(parents=True, exist_ok=True)
+
+        pic = Image.open(src)
+        out = apply_border(pic)
+        if not DEBUG:
+            out.save(dest)
+
+    return
+
+
+def apply_border(pic: Image.Image) -> Image.Image:
     sz = max(pic.size)
     bg = pic.resize((sz, sz))
     if DEBUG:
@@ -39,9 +99,7 @@ def main():
     if DEBUG:
         out.show()
 
-    if not DEBUG:
-        out.save(dest)
-    return
+    return out
 
 
 if __name__ == "__main__":
